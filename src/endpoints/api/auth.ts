741236080161f67567c382ff021ac12420c50536
William Harrison <william@williamdavidharrison.com.au>
2024-08-26 19:56:18 +0800

    change to wdh.app subdomain

--- src/endpoints/api/auth.ts
+++ src/endpoints/api/auth.ts
@@ -5,7 +5,7 @@ import querystring from "node:querystring";
 export default async (req: Request, res: Response) => {
     const params = querystring.stringify({
         client_id: process.env.github_client_id,
-        redirect_uri: "https://my-github-profile.wdh.gg/api/callback",
+        redirect_uri: "https://my-github-profile.wdh.app/api/callback",
         scope: "read:org read:user user:email"
     })
 
